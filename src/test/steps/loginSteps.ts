4d1fcb2075d67dfdfcbaa4a9412a48367dd8b640
Adam <adamcegielka@tlen.pl>
2023-08-24 19:41:21 +0200

    tests(fix): correct typo and add timeout

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,8 +1,10 @@
-import { Given, When, Then } from '@cucumber/cucumber';
+import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { chromium, Page, Browser, expect } from '@playwright/test';
 
 let browser: Browser;
 let page: Page;
+
+setDefaultTimeout(60 * 1000 * 2);
 
 Given('User navigates to the application', async function () {
   browser = await chromium.launch({ headless: false });
@@ -13,6 +15,7 @@ Given('User navigates to the application', async function () {
 
 Given('User click on the login link', async function () {
   await page.locator('//span[text()="Login"]').click();
+  // await page.locator('.mat-focus-indicator.mat-button.mat-button-base.ng-star-inserted').click();
 });
 
 Given('User enter the username as {string}', async function (username) {
@@ -24,12 +27,14 @@ Given('User enter the password as {string}', async function (password) {
 });
 
 When('User click on the login button', async function () {
-  await page.locator('button[color="primry"]').click();
+  await page.locator('button[color="primary"]').click();
+  // await page.locator('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click();
+
 });
 
 Then('Login should be success', async function () {
-  const textMessage = await page.locator('//button[contains(@class, "mat-focus-indicator mat-menu-trigger")]//span[1]').textContent();
-  console.log("Username: " + textMessage);
+  const textMessage = await page.locator('//button[contains(@class,"mat-focus-indicator mat-menu-trigger")]//span[1]').textContent();
+  console.log('Username: ' + textMessage);
   await browser.close();
 });
 
