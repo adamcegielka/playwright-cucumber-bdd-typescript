adbbfdf89bd2486aa4aa3835606c71e91dcca7ea
Adam <adamcegielka@tlen.pl>
2023-08-26 11:45:47 +0200

    tests(fix): fix error wirh `page`

--- src/hooks/pageFixture.ts
+++ src/hooks/pageFixture.ts
@@ -1,6 +1,5 @@
 import { Page } from '@playwright/test';
-import { Logger } from '@playwright/test';
 
 export const pageFixture = {
-  page: undefined as Page,
+  page: undefined as unknown as Page,
 };
