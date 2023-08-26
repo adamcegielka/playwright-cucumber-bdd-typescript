1318cae2284979d92e46ffd4b51b3863f1122e8a
Adam <adamcegielka@tlen.pl>
2023-08-26 11:07:04 +0200

    tests(feat): add hooks

---
+++ hooks/hooks.ts
@@ -0,0 +1,9 @@
+import { BeforeAll, AfterAll, Before, After } from '@cucumber/cucumber';
+
+BeforeAll(async () => {});
+
+AfterAll(async () => {});
+
+Before(async () => {});
+
+After(async () => {});
