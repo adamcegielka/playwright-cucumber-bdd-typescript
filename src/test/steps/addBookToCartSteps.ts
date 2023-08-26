2e0aa92033c70b3094c45b38a6be9609c6b81e77
Adam <adamcegielka@tlen.pl>
2023-08-26 11:40:49 +0200

    refactor: improve code structure

--- src/test/steps/addBookToCartSteps.ts
+++ src/test/steps/addBookToCartSteps.ts
@@ -1,20 +1,22 @@
 import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
-import { Page, expect } from '@playwright/test';
-
-let page: Page;
+import { expect } from '@playwright/test';
+import { pageFixture } from '../../hooks/pageFixture';
 
 setDefaultTimeout(60 * 1000 * 2);
 
 Given('user search for a {string}', async function (book) {
-  await page.locator('input[type="search"]').type(book);
-  await page.locator('mat-option[role="option"] span').click();
+  await pageFixture.page.locator('input[type="search"]').type(book);
+  await pageFixture.page.waitForTimeout(5000);
+  await pageFixture.page.locator('mat-option[role="option"] span').click();
 });
 
 When('user add the book to the cart', async function () {
-  await page.locator('//button[@color="primary"]').click();
+  await pageFixture.page.locator('//button[@color="primary"]').click();
 });
 
 Then('the cart badge should get updated', async function () {
-  const countInCart = await page.locator('#mat-badge-content-0').textContent();
-  expect(Number(countInCart?.length)).toBeGreaterThan(0);
+  const countInCart = await pageFixture.page
+    .locator('#mat-badge-content-0')
+    .textContent();
+  expect(Number(countInCart)).toBeGreaterThan(0);
 });
--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,45 +1,45 @@
 import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
-import { chromium, Page, Browser, expect } from '@playwright/test';
-
-let browser: Browser;
-let page: Page;
+import { expect } from '@playwright/test';
+import { pageFixture } from '../../hooks/pageFixture';
 
 setDefaultTimeout(60 * 1000 * 2);
 
 Given('User navigates to the application', async function () {
-  browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
-
-  await page.goto('https://bookcart.azurewebsites.net/');
+  await pageFixture.page.goto('https://bookcart.azurewebsites.net/');
 });
 
 Given('User click on the login link', async function () {
-  await page.locator('//span[text()="Login"]').click();
-  // await page.locator('.mat-focus-indicator.mat-button.mat-button-base.ng-star-inserted').click();
+  await pageFixture.page.locator('//span[text()="Login"]').click();
+  // await pageFixture.page.locator('.mat-focus-indicator.mat-button.mat-button-base.ng-star-inserted').click();
 });
 
 Given('User enter the username as {string}', async function (username) {
-  await page.locator('input[formcontrolname="username"]').type(username);
+  await pageFixture.page
+    .locator('input[formcontrolname="username"]')
+    .type(username);
 });
 
 Given('User enter the password as {string}', async function (password) {
-  await page.locator('input[formcontrolname="password"]').type(password);
+  await pageFixture.page
+    .locator('input[formcontrolname="password"]')
+    .type(password);
 });
 
 When('User click on the login button', async function () {
-  await page.locator('button[color="primary"]').click();
-  // await page.locator('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click();
-
+  await pageFixture.page.locator('button[color="primary"]').click();
+  // await pageFixture.page.locator('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click();
 });
 
 Then('Login should be success', async function () {
-  const textMessage = await page.locator('//button[contains(@class,"mat-focus-indicator mat-menu-trigger")]//span[1]').textContent();
+  const textMessage = await pageFixture.page
+    .locator(
+      '//button[contains(@class,"mat-focus-indicator mat-menu-trigger")]//span[1]',
+    )
+    .textContent();
   console.log('Username: ' + textMessage);
-  await browser.close();
 });
 
 When('Login should fail', async function () {
-  const errorMessage = page.locator('mat-error[role="alert"]');
+  const errorMessage = pageFixture.page.locator('mat-error[role="alert"]');
   await expect(errorMessage).toBeVisible();
-  await browser.close();
 });
